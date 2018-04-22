241f2526d9024e0db5f7e93e8b5f055f62067800
rinky09 <gatlasneha09@gmail.com>
2018-04-22 15:19:48 -0500

    modifird

---
+++ polymorphism/src/poly.java
@@ -0,0 +1,30 @@
+class sample
+{
+	void add(int c,int d)
+	{
+		System.out.println("sum of two num:"+(c+d));
+	}
+	void add(long a,long b)
+	{
+		System.out.println("sum of two num:"+(a+b));
+	}
+	
+	void add(int a,int b,int c)
+	{
+		System.out.println("sum of two num:"+(a+b+c));
+	}
+	void add(float a,float b)
+	{
+		System.out.println("sum:"+(a+b));
+	}
+}
+ class poly {
+	 public static void main(String args[])
+	 {
+		 sample s=new sample();
+		 s.add(1000000000,2000000000);
+		 s.add(10,20,30 );
+		 s.add(1.1f, 2.2f);
+	 }
+
+}
